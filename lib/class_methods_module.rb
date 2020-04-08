647a65b9edda3768b6972549f8aa4f866c2f99f2
Micah McCallum <micahmccallum@outlook.com>
2020-04-08 20:59:15 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -2,12 +2,4 @@ module MetaDancing
  def metadata
    "This class produces objects that love to dance."
  end
-
- # class Dancer
- #   extend MetaDancing
- # end
- #
- # class Kid
- #   extend MetaDancing
- # end
 end
